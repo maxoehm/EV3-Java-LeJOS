914b1a592e10fb0d74b781bdab758b20afe696c8
felixlie <felix18lie@gmail.com>
2021-10-29 09:13:00 +0200

    some changes

--- src/main/java/example/robotics/ev3/main/MainClass.java
+++ src/main/java/example/robotics/ev3/main/MainClass.java
@@ -15,6 +15,11 @@ import org.slf4j.LoggerFactory;
 
 import java.awt.*;
 import java.awt.geom.Point2D;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -36,8 +41,39 @@ public class MainClass {
     private static HashMap map = new HashMap();
     private static int index = 0;
 
+    //static ServerSocket variable
+    private static ServerSocket server;
+    //socket server port on which it will listen
+    private static int port = 9876;
 
-    public static void main(final String[] args) {
+    public static void main(String args[]) throws IOException, ClassNotFoundException{
+            //create the socket server object
+            server = new ServerSocket(port);
+            //keep listens indefinitely until receives 'exit' call or program terminates
+            while(true){
+                System.out.println("Waiting for the client request");
+                //creating socket and waiting for client connection
+                Socket socket = server.accept();
+                //read from socket to ObjectInputStream object
+                ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+                //convert ObjectInputStream object to String
+                String message = (String) ois.readObject();
+                System.out.println("Command Received: " + message);
+                //create ObjectOutputStream object
+                ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+                //write object to Socket
+                oos.writeObject("Packaged received: "+message);
+                //close resources
+                ois.close();
+                oos.close();
+                interpretCommand(message);
+                //terminate the server if client sends exit request
+                if(message.equalsIgnoreCase("exit"))                 socket.close();
+                break;
+            }
+
+
+
 
         System.out.println("Initialise Motors");
 
@@ -72,6 +108,9 @@ public class MainClass {
 }
 
 
+    }
+
+    private static void interpretCommand(String message) {
     }
 
     private static Double x = 0.0;
