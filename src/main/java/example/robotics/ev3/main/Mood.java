ecc27bebc95c26ff4d4c62432a9b4aaed8f6d476
felixlie <felix18lie@gmail.com>
2021-10-29 08:10:05 +0200

    some changes

---
+++ src/main/java/example/robotics/ev3/main/Mood.java
@@ -0,0 +1,17 @@
+package example.robotics.ev3.main;
+
+import java.io.File;
+
+public class Mood {
+
+    private String moodName;
+
+    //For Audio data
+    private File audio;
+
+
+
+
+
+
+}
---
+++ src/main/java/example/robotics/ev3/main/PersonalityOtis.java
@@ -0,0 +1,12 @@
+package example.robotics.ev3.main;
+
+import java.math.BigInteger;
+
+public class PersonalityOtis {
+
+    private final int age = 69;
+    private final String name = "Otis";
+    private final String[] sex = {"Fluid", "Queer", "Divers"};
+
+
+}
